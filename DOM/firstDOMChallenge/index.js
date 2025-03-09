bec6b66af68f00b4022505540dd2efa9cf6af5a6
Rency Delos Santos <rencydelossantos020@gmail.com>
2025-03-09 12:50:42 +0800

    Introduction to DOM: Properties and Method of an Object, and how to select element and selectors of html

---
+++ DOM/firstDOMChallenge/index.js
@@ -0,0 +1,5 @@
+var changeThirdList = document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Rency";
+changeThirdList;
+
+var changeButtonText = document.firstElementChild.lastElementChild.querySelector("button").innerHTML = "Don't Click me";
+changeButtonText;
