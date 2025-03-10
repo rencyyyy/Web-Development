8b292757fdf99b0d7069faf03cea482d2364a5f8
Rency Delos Santos <rencydelossantos020@gmail.com>
2025-03-10 12:21:24 +0800

    Selecting HTML Elements with Javascript added

--- DOM/firstDOMChallenge/index.js
+++ DOM/firstDOMChallenge/index.js
@@ -3,3 +3,9 @@ changeThirdList;
 
 var changeButtonText = document.firstElementChild.lastElementChild.querySelector("button").innerHTML = "Don't Click me";
 changeButtonText;
+
+var changeGoogleColor = document.querySelector("#list a").style.color = "orange";
+changeGoogleColor;
+
+// This will only change the bullet point without changing thw color of the text.
+document.querySelector("li").style.color = "purple";
