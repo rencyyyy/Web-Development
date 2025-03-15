d0a50dde492e089af335258f8ba12702e904ea72
Rency Delos Santos <rencydelossantos020@gmail.com>
2025-03-15 11:06:40 +0800

    How to play sound in website && Using Switch Statement in JavaScript

--- Drum-Kit/Drum Kit Starting Files/index.js
+++ Drum-Kit/Drum Kit Starting Files/index.js
@@ -15,9 +15,51 @@
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert("Hello World!");
+
+    var buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom1 = new Audio("./sounds/tom-2.mp3");
+            tom1.play();
+            break;
+        case "s":
+            var tom1 = new Audio("./sounds/tom-3.mp3");
+            tom1.play();
+            break;
+        case "d":
+            var tom1 = new Audio("./sounds/tom-4.mp3");
+            tom1.play();
+            break;
+        case "j":
+            var tom1 = new Audio("./sounds/snare.mp3");
+            tom1.play();
+            break;
+        case "k":
+            var tom1 = new Audio("./sounds/crash.mp3");
+            tom1.play();
+            break;
+        case "l":
+            var tom1 = new Audio("./sounds/kick-bass.mp3");
+            tom1.play();
+            break;
+        default: console.log(buttonInnerHTML);
+    }
+
     });
 }
 
 
+// var audio = new Audio("./sounds/crash.mp3");
+//         return audio.play();
+
+// var buttonColor = this.style.color;
+    // this.style.color = "white";
+    // setTimeout(() => {
+    //     this.style.color = buttonColor; // Revert to the original color after 0.5s
+    // }, 200);
 
