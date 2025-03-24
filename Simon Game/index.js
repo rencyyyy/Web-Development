0a57dac8bf7e8c224c2f264a336df06a9b45a760
Rency Delos Santos <rencydelossantos020@gmail.com>
2025-03-24 14:30:47 +0800

    Simon game pt2

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,24 +1,55 @@
+buttonColours = ["green", "red", "yellow", "blue"];
 
-var buttonColours = ["red", "blue", "green", "yellow"];
 gamePattern = [];
+userClickedPattern = [];
+
+var started = false;
+var level = 0;
+
+
+$(document).on("keydown", function(){
+    if(!started) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").on("click", function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    // console.log(userClickedPattern);
+
+});
 
 function nextSequence() {
+
+    level++;
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(50).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
 
-    var buttonSound = new Audio("sounds/" + randomChosenColour + ".mp3");
-    buttonSound.play();
+    
 }
-nextSequence();
 
+function playSound(name) {
+    var sound = new Audio("./sounds/" + name + ".mp3");
+    sound.play();
+}
 
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
 
+    setTimeout(function(){
+        $("#" + currentColour).removeClass("pressed");
+    },100);
+}
 
-
-
-
-
-
+// console.log(gamePattern);
