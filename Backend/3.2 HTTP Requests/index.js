a7465e17d8e6dffbd151e0339e000bca3f9084cc
Rency Delos Santos <rencydelossantos020@gmail.com>
2025-04-05 19:25:25 +0800

    Added enpoints

--- Backend/3.2 HTTP Requests/index.js
+++ Backend/3.2 HTTP Requests/index.js
@@ -5,6 +5,19 @@ app.get("/", (req,res) => {
     res.send("Hello HTTP Requests!");
 });
 
+app.get("/home", (req,res) => {
+    res.send("<h1>This is a Home Page</h1>");
+});
+
+app.get("/about", (req,res) => {
+    res.send("<h1>About Page</h1>");
+});
+
+app.get("/contact", (req,res) => {
+    res.send("<h1>This is a Contact Page</h1></fr><p>Smart: 09813082969</p><p>Globe: 09dikoalamnakalimutanko</p>");
+});
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000.");
 });
